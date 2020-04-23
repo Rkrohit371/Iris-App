54bec9889ff050fc6bcfaace4600fa7718664983
rohit <rohitkg1998@gmail.com>
2020-04-23 21:25:39 +0530

    Iris EDA App Deployment

---
+++ README.md
@@ -0,0 +1,8 @@
+# IRIS-EDA-APP
+A simple EDA App with STREAMLIT
+
+
+#Required Files
+1. Setup.sh
+2. Procfile
+3. requirements.txt
---
+++ iris_eda_app.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+
+def main():
+	"""Simple Iris EDA App"""
+st.title("Iris EDA App with Streamlit")
+st.subheader("Streamlit Deployed App")
+
+
+if __name__ == '__main__':
+	main()
